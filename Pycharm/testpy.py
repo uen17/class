445ac231a133f3e11860b24b79e0d4fa06674f4f
824618415 <824618415@qq.com>
2020-10-26 07:27:17 +0800

    ivjsbihw

--- Pycharm/testpy.py
+++ Pycharm/testpy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 class UserInfo(object):
@@ -12,11 +12,26 @@ class UserInfo(object):
     def get_account(self):
         return self.__account
 
+    @classmethod
+    def get_name(cls):
+        return cls.lv
+
+    @property
+    def get_age(self):
+        return self._age
+
 
 class UserInfo2(UserInfo):
-    pass
+    def __init__(self, name, age, account, sex):
+        super(UserInfo2, self).__init__(name, age, account)
+        self.sex = sex
 
 
 if __name__ == '__main__':
-    userInfo2 = UserInfo2('两点水', 23, 347073565);
-    print(userInfo2.get_account())
+    userInfo2 = UserInfo2('两点水', 23, 347073565, '男')
+    # 打印所有属性
+    print(dir(userInfo2))
+    # 打印构造函数中的属性
+    print(userInfo2.__dict__)
+    print(UserInfo2.get_name())
+
