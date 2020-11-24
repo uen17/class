f68cbb2dd1bfc3f15eb7515915f2441a26c7b33e
824618415 <824618415@qq.com>
2020-11-24 14:05:23 +0800

    script sync

--- IDEA/src/com/company/Main.java
+++ IDEA/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println(" test java");
+
+    int rel = plus(10,29);
+    System.out.print("self(9) + (10 + 20) = ");
+	System.out.println(rel);
+
+    }
+    public static int plus(int v1, int v2){
+        return v1 + v2;
     }
 }
